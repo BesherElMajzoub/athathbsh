7c8a42e2c0c9163f1f3a31768974dfc3d0d86a4b
BesherElMajzoub <88548828+BesherElMajzoub@users.noreply.github.com>
2026-02-08 13:42:56 +0100

    edit

--- vendor/autoload.php
+++ vendor/autoload.php
@@ -19,4 +19,4 @@ if (PHP_VERSION_ID < 50600) {
 
 require_once __DIR__ . '/composer/autoload_real.php';
 
-return ComposerAutoloaderInitc514d8f7b9fc5970bdd94287905ef584::getLoader();
+return ComposerAutoloaderInit7a9a555953dd4c7b27d912d83d81fb0c::getLoader();

198853c31038bc564d4e8484bef44dfb83eeea62
Besher ElMajzoub <beshermajzou@gmail.com>
2026-02-22 06:34:46 +0300

    Update site.php

--- config/site.php
+++ config/site.php
@@ -4,7 +4,7 @@ return [
     'site_name_ar' => env('SITE_NAME_AR', 'شراءاثاث مستعمل فورا'),
     'phone' => env('SITE_PHONE', '0508232513'),
     'phone_display' => env('SITE_PHONE_DISPLAY', '0508232513'),
-    'whatsapp_number_international' => env('SITE_WHATSAPP_INTERNATIONAL', '0508232513'),
+    'whatsapp_number_international' => env('SITE_WHATSAPP_INTERNATIONAL', '+966508232513'),
     'whatsapp_default_message' => env('SITE_WHATSAPP_MESSAGE', 'السلام عليكم، أبغى استفسر عن شراء الأثاث المستعمل.'),
     'city_name_ar' => env('SITE_CITY_AR', 'جدة'),
     'country_code' => env('SITE_COUNTRY_CODE', '966'),
